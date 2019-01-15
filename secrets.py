69716fd8e242f442b5320ec77eab1e3b480711a5
jjproby <jjproby719@gmail.com>
2019-01-15 11:11:51 -0600

    first couple files. Includes API keys to Twitter

---
+++ secrets.py
@@ -0,0 +1,7 @@
+#secrets.py
+
+consumer_key = 'x8yoIKKx2XW47tV6DVrvBSL7e'
+consumer_secret = 'mCMiWQr3rwLbb5G0xFdr0C5RNqbX33KnyAFhtEgLNS87pcrOxL'
+
+access_token = '956985013253353473-AMt2TjQc92YHP7sGOLXOVJg2CdukKvY'
+access_secret = 'rUIYWjY3xA3FIIJNvqWoiMJTquPnhoBCwdps5VjUWyPiN'
