1b33d19019ff764e92cc7769854339e0730ece7b
jjproby <jjproby719@gmail.com>
2019-01-15 16:56:09 -0600

    Just added some twitter streaming code into it

--- bot.py
+++ bot.py
@@ -3,6 +3,20 @@
 import tweepy
 from secrets import *
 
+# create a class inherithing from the tweepy StreamListener
+class BotStreamer(tweepy.StreamListener):
+
+    # Called when a new status arrives which is passed down from the on_data method of the StreamListener
+    def on_status(self, status):
+        username = status.user.screen_name
+        status_id = status.id
+
+    #entities provide structured data from Tweets including resolved URLs, media, hashtags
+    #and mentions without having to parse the text to extract that information
+        if 'media' in status.entities:
+            for image in status.entities['media']:
+                tweet_image(image['media_url'], username, status_id)
+
 #create an OAuthHandler instance
 #Twitter requires all requests to use OAuth
 
@@ -14,8 +28,15 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth) #creates API object
 
-#reads tweets in timeline
+#constructs the Stream instance
+myStreamListener = BotStreamer()
 
+stream = tweepy.Stream(auth, myStreamListener)
+# filter all tweets tweeted at the bot
+stream.filter(track=['@capstonebot'])
+
+
+#reads tweets in timeline
 
 public_tweets = api.user_timeline('Lin_Manuel')
 for tweet in public_tweets:
