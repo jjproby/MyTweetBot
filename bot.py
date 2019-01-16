f7f5c649e3b2acaf68356025e13a4661a9048d97
jjproby <jjproby719@gmail.com>
2019-01-15 20:43:02 -0600

    Made the image scrambler! Now to add more features! Will do word of the day next!

--- bot.py
+++ bot.py
@@ -2,6 +2,53 @@
 
 import tweepy
 from secrets import *
+import requests
+from io import BytesIO
+from PIL import Image
+import random
+
+def tweet_image(url, username, status_id):
+    filename = 'temp.png'
+    #send a get request
+    request = requests.get(url, stream = True)
+    if request.status_code == 200:
+        #read data from downloaded bytes and return a PIL.Image.Image object
+        i = Image.open(BytesIO(request.content))
+        #Saves the image under the given filename
+        i.save(filename)
+        scramble(filename)
+        #Update the authenticated user's status
+        api.update_with_media('scramble.png', status='@{0}'.format(username), in_replp_to_status_id=status_id)
+    else:
+        print("unable to download image")
+
+
+def scramble(filename):
+    BLOCKLEN = 64
+
+    img = Image.open(filename)
+    width, height = img.size
+
+    xblock = width // BLOCKLEN
+    yblock = height // BLOCKLEN
+    # creates sequence of 4-tuples (box) defining the left, upper, right, and lower pixel coordinate
+    blockmap = [(xb * BLOCKLEN, yb * BLOCKLEN, (xb + 1) * BLOCKLEN, (yb + 1) * BLOCKLEN)
+                for xb in range(xblock) for yb in range(yblock)]
+
+    shuffle = list(blockmap)
+
+    #shuffle the sequence
+    random.shuffle(shuffle)
+
+    # Creates a new image with given mode and size
+    result = Image.new(img.mode, (width, height))
+    for box, sbox in zip(blockmap, shuffle):
+        #returns a rectangular region from this original image.
+        crop = img.crop(sbox)
+        # Pastes the cropped pixel into the new image Object
+        result.paste(crop, box)
+    result.save('scramble.png')
+
 
 # create a class inherithing from the tweepy StreamListener
 class BotStreamer(tweepy.StreamListener):
@@ -16,6 +63,7 @@ class BotStreamer(tweepy.StreamListener):
         if 'media' in status.entities:
             for image in status.entities['media']:
                 tweet_image(image['media_url'], username, status_id)
+
 
 #create an OAuthHandler instance
 #Twitter requires all requests to use OAuth
@@ -33,12 +81,6 @@ myStreamListener = BotStreamer()
 
 stream = tweepy.Stream(auth, myStreamListener)
 # filter all tweets tweeted at the bot
-stream.filter(track=['@capstonebot'])
+stream.filter(track=['@jjproby719'])
 
-
-#reads tweets in timeline
-
-public_tweets = api.user_timeline('Lin_Manuel')
-for tweet in public_tweets:
-    tweet.text = tweet.text.encode('utf-8')
-    print(tweet.text)
+api.update_status("I am a bot and I did a thing!")
