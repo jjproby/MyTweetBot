507925b6d91c1cca8c7699b638785297d393f072
jjproby <jjproby719@gmail.com>
2019-03-01 11:14:03 -0600

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,14 +1,14 @@
 #secrets.py
 
-consumer_key = 'rVT4uRELl016dAah4EiJQbq5H'
-consumer_secret = 'Hg37xzpUFU2D6LszNlPfCyhcVincmomTkVQAftYEy8ZfBl7tue'
+consumer_key = 'TWITTER_API_KEY'
+consumer_secret = 'TWITTER_API_SECRET_KEY'
 
-access_token = '956985013253353473-s6mzrS00jSxrhlWbYSs8qmd0zNZ0HMU'
-access_secret = 'VbdhfqFuyTSx56BAAsPx2lkRElu2vuRJJ97i8LlDiV01E'
+access_token = 'TWITTER_ACCESS_TOKEN'
+access_secret = 'TWITTER_ACCESS_TOKEN'
 
 apiUrl = 'http://api.wordnik.com/v4'
-apiKey = '9b827dd68c55dc3fd010e05126d08aaeedeec622e6ea8ed29'
+apiKey = 'WORDNIK_API_KEY'
 
-genius_id = "xlR5Hd3WFF0fEKJqnYO97I5LqP-JS4Tz-mEup4rF2EC6WfGh-4ULgTBcldu0HsWE"
+genius_id = "GENIUS_API_ID"
 
-genius_key = "EMEdBODhr5_8NEPk7ViBVbw4H7zNwDmyirXXmXDAJMaf35mCnCt_sfDkrYUk12YMZEP-K55wp4fz1DiE4OjxLQ"
+genius_key = "GENIUS_API_KEY"
